08f87ff7a079300a5d42876e3b32c33a0dae21d0
Iordan Tonchev <issashu@icloud.com>
2021-01-29 09:40:53 +0200

    work in class

--- LectureTest.c
+++ LectureTest.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-int main(){
-    char c='!';
-    while(c!='?'){
-        c=getchar();
-        if((c>=97 && c<=122) || (c>=65 && c<=90)){
-            putchar(c);
-        };
-    }
-    return 0;
+int main()
+{
+    const char ccMyConst ='\xc';
+    printf("%d", ccMyConst);
 }
