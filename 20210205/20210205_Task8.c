365c2c44299ad8d127aa330fa6e71ca29e28a7b4
Iordan Tonchev <issashu@icloud.com>
2021-02-06 18:39:45 +0200

    task8

---
+++ 20210205/20210205_Task8.c
@@ -0,0 +1,87 @@
+/*
+Задача 8. Напишете функците atoi, itoa, atof и reverse, които писахме преди като използвате указател 
+вместо индексиране на масив.
+
+Във ВС Код задачата дава грешка (поне при мен), заради това, ето пак линк към Коуд Колаб,
+където си работи:
+
+https://codecollab.io/@issashu/Lessons
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(char *s);
+double atof(char *s);
+int atoi(char *s);
+
+int main(){
+    char String1[] = "Iordan";
+    char String2[] = "1234.567890";
+
+    reverse(String1);
+    printf("%s\n", String1);
+    printf("%lf\n", atof(String2));
+
+
+}
+
+void reverse(char *s){
+/* reverse has two pointers: One at begining and one at end of string. They swap places via proxy
+variable, until they meet at the middle.*/
+    int counter=0;
+    char temp = 0;
+    char *start = s;
+    char *end = s;
+    while( *(s+counter) != '\0'){
+        counter++;
+    }
+    for (int c = 0 ; c < ( counter - 1 ) ; c++ ){
+        end++;
+    }
+
+    for (int i = 0 ; i < counter/2 ; i++ ) {        
+      temp = *end;
+      *end = *start;
+      *start = temp;
+ 
+      start++;
+      end--;
+   }
+}
+
+double atof(char *s){ 
+    int i=0;
+    int counter=0;
+    int power = 1;
+    double n = 0.00;
+    while ( *(s+counter) !='.'){
+        counter++;
+    }
+    for(; *(s+counter) >= '0' && *(s+counter) <= '9' && *(s+counter)!='.'; s++)
+        n = 10 * n + (*(s+counter) - '0');
+    
+    for (i=counter; *(s+i)!='\0';i++){
+        n = 10 * n + (*(s+i) - '0');
+        power*=10;
+    }
+    return (n/power);    
+ }
+
+ int atoi(char *s){ 
+    int i=0;
+    int counter=0;
+    int power = 1;
+    double n = 0.00;
+    while ( *(s+counter) !='.'){
+        counter++;
+    }
+    for(; *(s+counter) >= '0' && *(s+counter) <= '9' && *(s+counter)!='.'; s++)
+        n = 10 * n + (*(s+counter) - '0');
+    
+    for (i=counter; *(s+i)!='\0';i++){
+        n = 10 * n + (*(s+i) - '0');
+        power*=10;
+    }
+    return (n/power);    
+ }
--- LectureTest.c
+++ LectureTest.c
@@ -1,19 +1,69 @@
+/*Задача 8. Напишете функците atoi, itoa, atof и reverse, които писахме
+преди като използвате указател вместо индексиране на масив.*/
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-void printStr(char *s);
+int atoi(char *str);
+void itoa(int n, char *str);
+void reverse(char *str);
+int atof(char *str);
 
-int main(){
-    char string[] = {"Hello world! Did you bring a towel?"};
-    char *s = &string[0];
-    printStr(s);
+int main(void) {
+    char s[] = {"234"};
+    char t[10];
+    int n = 24;
 
+    printf("%d\n", atoi(s));
+    itoa(n, t);
+    reverse(t);
+    printf("%s", t);
+
+    return 0;
+}
+void itoa(int n, char *str) {
+    int sign;
+
+	if((sign = n) < 0)	/* record sign */
+		n = -n;			/* make n positive */
+
+	do {				
+		*str = n % 10 + '0'; 
+        str++;	
+	} while((n /= 10) > 0);		/* delete it */
+	*str = '\0';
+    return;
 }
 
-void printStr(char *s){
-    for(int i=0; s[i]!='\0'; i++){
-        printf("%c", s[i]);
-    }
-    printf("\n");
+void reverse(char *str) {
+   int length, c;
+   char *begin, *end, temp;
+ 
+   length = strlen(str);
+ 
+   begin = str;
+   end = str;
+ 
+   for (c = 0 ;c < (length - 1);c++)
+      end++;
+ 
+   for (c = 0 ; c < length/2; c++) 
+   {        
+      temp = *end;
+      *end = *begin;
+      *begin = temp;
+ 
+      begin++;
+      end--;
+   }
+   return;
+}
+
+int atoi(char *str) {
+    int res = 0;
+    
+    while(*str){
+        res = res * 10 + *str - '0';
+        str++;
+    } //234
+    return res;
 }
