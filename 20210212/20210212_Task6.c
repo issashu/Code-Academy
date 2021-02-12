3f858483a4f4529d741c488f20d0c2191865d835
Iordan Tonchev <issashu@live.com>
2021-02-12 12:56:34 +0200

    6 update

--- 20210212/20210212_Task6.c
+++ 20210212/20210212_Task6.c
@@ -8,7 +8,10 @@
 Направена е със scanf. Не ми се получава с getchar() и прекъсване (CTRL+C просто прекъсва всичко). 
 Заради това съм сложил автоматично прекъсване, когато увеличим бъфера три поредни пъти.
 
-Логиката за увеличаване на отделената памет в хийп е обаче същата.
+Пак имам бъг, че брой enter като юн , но не съм сложил сега проверка да го игнорира.
+
+Логиката за увеличаване на отделената памет в хийп е обаче същата. Като се запълни, реалокира пойнтера на ново
+място в паметта и така 3 поредни пъти.
 */
 
 #include <stdio.h>
@@ -21,9 +24,13 @@ int main(){
     printf("Please enter the values for Pointy:\n");
     char *Pointy = malloc(max);
     while(Pointy && trigger < 3){
-        for(int i=0; i<max; i++){
+        for(i; i<max; i++){
+            scanf("%c", &Pointy[i]);
+            /*DEBUGGER:
             Pointy[i] = 'q';
-            if (i = max - 1) {
+            */
+            //1 chas!!!!!!!!! debugvah, dokato se usetia, che sam pisal =, a ne == ....
+            if (i == max - 1) {
             Pointy = realloc (Pointy, max+2);
                 if (!Pointy) {
                     printf ("Realloc failed!!!\n");
@@ -31,11 +38,14 @@ int main(){
             max += 2;
             trigger++;
             }
-            if(trigger==3)
+            if(trigger==3){
+                Pointy[i+1]='\0';
                 break;
+            }
+                
         }
     }
-    for (int i=0; i<max;i++){
-        printf("%c,", Pointy[i]);
+    for (int i=0; Pointy[i]!='\0';i++){
+        printf("%c", Pointy[i]);
     }
 }
