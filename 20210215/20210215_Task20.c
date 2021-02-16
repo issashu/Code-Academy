ff14b2470640a58644890b7bbbf17600b6b67d9e
Iordan Tonchev <issashu@live.com>
2021-02-16 15:48:31 +0200

    Update 20210215_Task20.c

--- 20210215/20210215_Task20.c
+++ 20210215/20210215_Task20.c
@@ -1,3 +1,28 @@
 /*
 Задача 20. Дефинирайте стринг „I am a poor programmer”. Заменете през пойнтер “poor” с “great”.
 */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char array[] = "I am a poor programmer";
+    char arraysubstitute [] = "great";
+
+    char *Pointy = &array[0];
+    char *SubPointy = strstr(array, "poor");
+    char *PointoPlace = arraysubstitute;
+    int Counter = 0;
+    if (SubPointy==NULL){
+        printf("String not found! Error! Eroor! Waaaah!");
+    }
+    else{
+        while(*SubPointy){
+            *SubPointy = *PointoPlace;
+            *SubPointy++;
+            *PointoPlace++;
+            Counter++;
+        }
+    }
+    printf("%s", Pointy);
+}
