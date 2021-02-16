9c793e2eeff18b2e9964fa8710b0f8e973a5deb5
Iordan Tonchev <issashu@live.com>
2021-02-16 12:49:47 +0200

    Update 20210212_Task11.c

--- 20210212/20210212_Task11.c
+++ 20210212/20210212_Task11.c
@@ -19,22 +19,17 @@ void IncreaseDB (char **Participants, int *Size, int *NameLength);
 void PrintDB (char **Participants, int *Size, int *NameLength);
 
 int main(){
+    int i=0;
  int DBEntries = 2;
  int StrLen = 30;
  int NrColumns = 3;
  int *DBSize = &DBEntries;
  int *NameLength = &StrLen;
- char **Participants = calloc(DBEntries, sizeof *Participants);
- for (int i = 0; i<DBEntries; i++){
-        Participants[i] = calloc(NrColumns, StrLen * sizeof(char));
+ char **Participants = (char**) malloc (DBEntries * sizeof (char *));
+ for ( i = 0; i<DBEntries; i++){
+        Participants[i] = (char*) malloc (NrColumns * sizeof(char));
     }
-    EnterParticipant (Participants, DBSize, NameLength);
-    PrintDB(Participants, DBSize, NameLength);
-    free(Participants);
-}
-
-void EnterParticipant (char **Participants, int *Size, int *NameLength){
-    static int i = 0;
+    //EnterParticipant (Participants, DBSize, NameLength);
     char Answer ='y';
     do{
         printf("Please enter the Participant Name: \n");
@@ -44,25 +39,29 @@ void EnterParticipant (char **Participants, int *Size, int *NameLength){
         printf("Please enter the Participant Age: \n");
         fgets(&Participants[i][2], *NameLength, stdin);
         i++;
-            if(i == (*Size-2)){
-                *Size += *Size;
-                IncreaseDB(Participants, Size, NameLength);
+            if(i == (*DBSize-2)){
+                *DBSize += *DBSize;
+                Participants = realloc(Participants, *DBSize);
             }
         printf("Do you want to add further participants? (Press y for yes, anything else for no)\n");
         Answer = getchar();
     } while(Answer=='y');
-}
 
-void IncreaseDB (char **Participants, int *Size, int *NameLength){
-    Participants = realloc(Participants, *Size);
-}
-void PrintDB (char **Participants, int *Size, int *NameLength){
-    for(int i=0; i<*Size; i++){
+    for(int i=0; i<*DBSize; i++){
         for(int j=0; j<=3; j++){
             printf("%s ", Participants[i][j]);
         }
         printf("\n");
     }
+
+    for(int i=0; i<*DBSize; i++){
+        for(int j=0; j<=3; j++){
+            printf("%s ", Participants[i][j]);
+        }
+        printf("\n");
+    }
+    
+    free(Participants);
 }
 
 
