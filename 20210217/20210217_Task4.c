9dc3046dd5a47d4abd6dd6b5147b86446bb45f17
Iordan Tonchev <73355148+issashu@users.noreply.github.com>
2021-02-17 20:34:31 +0200

    Update task 4
    Stack finally works

--- 20210217/20210217_Task4.c
+++ 20210217/20210217_Task4.c
@@ -12,19 +12,24 @@ int pop() (премахва елемент от края на стека)
 Ако искаме Queue, тогава си правим FIFO вместо LIFO
 
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int CreateStack(int *StackPointer, int Size);
-void push(int *StackPointer, int Element, int Size);
+
+
+// int CreateStack(int *StackPointer, int Size);
+void push(int *StackPointer, int *Position);
 void StackCounter (int *Position, int Direction);
-int pop();
+int pop(int *StackPointer, int *Position);
 
 int main(){
-    int MaxStackSize = 12;
+    int MaxStackSize = 0;
     int StackPosition = 0;
+    int Operation = 0;
+    char Continue ='y';
+    int MaxRamAllowed = 1073741824;
     int *Stack = NULL;
+
 
     printf("How big do you need the stack?\n");
     scanf("%d", &MaxStackSize);
@@ -36,6 +41,38 @@ int main(){
     
     printf("%p\n", Stack);
     printf("%d\n", MaxStackSize);
+
+    while( Continue == 'y'){
+        printf("Press 1 for adding an element on the stack or 2 for removing:\n");
+        scanf("%d", &Operation);
+
+        if (Operation == 1 && StackPosition == MaxStackSize){
+            MaxStackSize *= 2;
+            if (sizeof(Stack) >= MaxRamAllowed){
+                printf("We are out of RAM dimension! May God have mercy on our CPUs!");
+            }
+            else{
+                Stack = realloc (Stack, MaxStackSize*sizeof(int));
+                push(Stack, &StackPosition);
+            }
+            
+        }
+        else if(Operation == 1 && StackPosition >= 0){
+            push(Stack, &StackPosition);
+        }
+        else if(Operation == 2 && StackPosition > 0){
+            pop(Stack, &StackPosition);
+        }
+        else if(Operation == 2 && StackPosition == 0){
+            printf("Stack is empty. Nothing to remove!");
+        }
+        
+        printf("\nDo you want to continue? (y for yes, anykey for no)\n");
+        scanf (" %c", &Continue);
+        // space before % to counter whatever retarded C buffer enter goes in....
+    }
+
+    free (Stack);
 }
 
 /*int CreateStack(int *StackPointer, int Size){
@@ -47,15 +84,31 @@ int main(){
     return *StackPointer;
 }*/
 
-void push(int *StackPointer, int Element, int Size){
-    int AddDirection = 1;
-
-    
+void push(int *StackPointer, int *Position){
+    int PushDirection = 1;
+    int Element = 0;
+    if(Position >=0){
+        printf("Please enter a number to push to the stack:\n");
+        scanf("%d", &Element);
+        StackPointer[*Position] = Element;
+        StackCounter(Position, PushDirection);
+        printf("Stack currently contains:\n");
+        for(int i = 0; i<*Position; i++){
+            printf("%d, ", StackPointer[i]);
+        }
+    }
 }
 
-int pop(){
+int pop(int *StackPointer, int *Position){
     int PopDirection = -1;
-
+    if (Position >= 0){
+        StackPointer[*Position] = '\0';
+        StackCounter(Position, PopDirection);
+    }
+    printf("Stack currently contains:\n");
+        for(int i = 0; i<*Position; i++){
+            printf("%d, ", StackPointer[i]);
+        }
 }
 
 void StackCounter (int *Position, int Direction){
