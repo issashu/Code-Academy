7ebfb883039ad73645a871b8b72d8e3f8d34400f
Iordan Tonchev <73355148+issashu@users.noreply.github.com>
2021-02-22 14:50:56 +0200

    Update LectureTest.c

--- LectureTest.c
+++ LectureTest.c
@@ -1,18 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-enum bool{false, true};
-
-int main(void){
-  enum bool b = 0;
-  switch (b){
-    case true:
-    printf("True");
-    break;
-
-    case false:
-    printf("False");
-    break;
-  }
-return 0;
+#include <string.h>
+ 
+int countLetters(char* p);
+ 
+int main(int argc, char **argv) {
+    char arr[1000] = {0};
+    char c;
+    int i = 0;
+ 
+    printf("Enter a message/text: ");
+ 
+    while ((c = getchar()) != '\n'){
+        arr[i++] = c;
+    }
+ 
+    int length = strlen(arr);
+    char *p = *&arr;
+    int (*res)(char) = countLetters;
+ 
+    printf("Count of 'a' in the string is: %d", (*res)(p));
+ 
+    return 0;
+}
+ 
+int countLetters(char *p){
+    int counter = 0;
+    char charA = 'a';
+ 
+    while (*p != '\0' ) {
+       if(*p == charA)
+            counter++;
+    }
+ 
+    return counter;
 }
