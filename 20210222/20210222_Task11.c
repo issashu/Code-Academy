b915bc79912ef92fc417c7afc3a8d0e6e3663dff
Iordan Tonchev <issashu@live.com>
2021-02-23 16:37:06 +0200

    updfate 11

--- 20210222/20210222_Task11.c
+++ 20210222/20210222_Task11.c
@@ -6,7 +6,26 @@
 */
 
 #include <stdio.h>
+#include <stdint.h>
 
-union Obedinenie{
-    int
+typedef union together{
+    int Var1;
+    int Var2;
+    char Var3;
+};
+
+int main(){
+    union together Obedineni ={0, 0, "c"};
+
+    Obedineni.Var3 = "c";
+    printf("The char variable is: %s\n", Obedineni.Var3);
+
+    Obedineni.Var1 = -7;
+    printf("The int 8bit variable is: %d\n", Obedineni.Var1);
+    printf("The char variable is: %s\n", Obedineni.Var3);
+
+    Obedineni.Var2 = 10;
+    printf("The 64bit int variable is: %d\n", Obedineni.Var2);
+    printf("The int 8bit variable is: %d\n", Obedineni.Var1);
+    printf("The char variable is: %s\n", Obedineni.Var3);
 }
