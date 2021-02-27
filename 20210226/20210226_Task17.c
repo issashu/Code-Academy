49f4768db2d1a2062e01dcfafc348fae1d6a3487
Iordan Tonchev <issashu@live.com>
2021-02-27 17:57:22 +0200

    Create 20210226_Task17.c

---
+++ 20210226/20210226_Task17.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define BroiDini 3
+
+typedef struct{
+    float Diametar;
+    float Kora;
+}dinia;
+
+float randomize(int lower, int upper);
+float average(dinia Ptr);
+
+int main(){
+    srand(time(0));
+
+    int upper = 35;
+    int lower = 5;
+    int count = 3;
+
+    dinia *A = malloc(BroiDini*sizeof(dinia));
+    dinia *B = malloc(BroiDini*sizeof(dinia));
+
+    for (int i=0 ; i<BroiDini; i++){
+        A[i].Diametar = (float) randomize (15, 140);
+        B[i].Diametar = (float) randomize (35, 95);
+
+        A[i].Kora = (float) randomize (5, 35)/10;
+        B[i].Kora = (float) randomize (3, 9)/10;
+    }
+    /*DEBUG:
+    for (int i=0 ; i<BroiDini; i++){
+        printf("A Diametar: %.1f\n", A[i].Diametar);
+        printf("B Diametar: %.1f\n", B[i].Diametar);
+
+        printf("A Kora: %.1f\n", A[i].Kora);
+        printf("B Kora: %.1f\n", B[i].Kora);
+    }
+    */
+}
+
+float randomize(int lower, int upper){
+    float num = (rand() % (upper - lower + 1)) + lower;
+
+    return num;
+}
+
+float average(dinia Ptr[]){
+    float Average = 0.00;
+    float Suma = 0.00;
+
+    for (int i =0; i<BroiDini; i++){
+        Suma += Ptr[i].Diametar;
+    }
+    Average = Ptr.Kora
+}
