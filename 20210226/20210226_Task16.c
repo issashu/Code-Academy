2f70af83a121a6d0cf2b1a1090930d6a5ae9252c
Iordan Tonchev <73355148+issashu@users.noreply.github.com>
2021-03-01 07:27:54 +0200

    26.02.2021 Team Homework LoTR

--- 20210226/20210226_Task16.c
+++ 20210226/20210226_Task16.c
@@ -1,45 +1,350 @@
+/*Task uses Parallel Array technique in a struct, which is there just for code convenience.
+Can be done with 3 independent atrrays.
+Using linear search to move through the arrays
+
+Detailed rules of movement: https://sites.google.com/site/boardandpieces/list-of-games/checker-solitaire 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+static const int BoardSize = 8;
 typedef struct{
     char Row1[8];
     char Row2[8];
     char Row3[8];
 } gameboard;
 
-void MovePiece();
-void SaveGame();
-void SetupGame(gameboard *Board);
+/*Set flag y or n on variables*/
+typedef struct{
+    bool North;
+    bool South;
+    bool West;
+    bool East;
+    bool Row1;
+    bool Row2;
+    bool Row3;
+} allowed;
+
+gameboard* SetupGame(gameboard *Board);
+gameboard* SaveGame(gameboard *MainBoard, gameboard *SaveBoard);
+gameboard* LoadGame(gameboard *MainBoard, gameboard *SaveBoard);
+int ValidateMovement(allowed *Directions, gameboard *MainBoard, char Piece);
+char ContinueGame (char Continue);
+void MovePiece(char Piece, char Direction, gameboard *MainBoard);
+void PrintBoard(gameboard *MainBoard);
+
+void Rules();
 void Printer();
 
 int main(){
+    char Piece = '1';
+    char Direction = 'N';
+    char LoadVar = 'n';
+    char Continue ='y';
+
     gameboard *MainBoard;
     gameboard *SaveBoard;
     MainBoard = SetupGame(MainBoard);
-    SaveBoard = SetupGame();
+    SaveBoard = SetupGame(SaveBoard);
 
+    Rules();
     /* Main Game Loop*/
     while(1){
-
-
+        /*We save the state, in case the user wants to go back. After we execute commands for movement of the pieces.*/
+        SaveBoard = SaveGame(MainBoard, SaveBoard);
+        PrintBoard(MainBoard);
+        Printer();
+        MovePiece(Piece, Direction, MainBoard);
+        PrintBoard(MainBoard);
+        printf("Do you need to turn back time? y/Y\n");
+        scanf(" %c", &LoadVar);
+        if(LoadVar == 'y' || LoadVar =='Y'){
+            printf("The wind whispers \"Rinn-\" and the world shifts around you!\n\n");
+            MainBoard = LoadGame(MainBoard, SaveBoard);
+        }
+        else{
+            printf("Frodo moves ever so close to mount Doom:\n- Curse us and crush us, my precious is lost!\n\n");
+        }
+        Continue = ContinueGame(Continue);
+        if(Continue=='n'){
+            printf("Farewell, King under the Mountain! This is a bitter adventure, if it must end so!\n");
+            break;
+        }
     }
 }
 
-void MovePiece(){
-
+int ValidateMovement(allowed *Directions, gameboard *MainBoard, char Piece){
+    /*Locate the chose number as position in the arrays*/
+    int i = 0;
+    for (int i=0; i<BoardSize; i++){
+        if(MainBoard->Row1[i] == Piece && (i>0&&i<BoardSize)){
+            Directions->North = false;
+            Directions->South = true;
+            Directions->West = true;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row1[i] == Piece && i==0){
+            Directions->North = false;
+            Directions->South = true;
+            Directions->West = true;
+            Directions ->East = false;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row1[i] == Piece && i==BoardSize){
+            Directions->North = false;
+            Directions->South = true;
+            Directions->West = false;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row2[i] == Piece && (i>0&&i<BoardSize)){
+            Directions->North = true;
+            Directions->South = true;
+            Directions->West = true;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = true;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row2[i] == Piece && i==0){
+            Directions->North = true;
+            Directions->South = true;
+            Directions->West = true;
+            Directions ->East = false;
+            Directions->Row1 = true;
+            Directions->Row2 = true;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row2[i] == Piece && i==BoardSize){
+            Directions->North = true;
+            Directions->South = true;
+            Directions->West = false;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = true;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row3[i] == Piece && (i>0&&i<BoardSize)){
+            Directions->North = true;
+            Directions->South = false;
+            Directions->West = true;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row3[i] == Piece && i==0){
+            Directions->North = true;
+            Directions->South = false;
+            Directions->West = true;
+            Directions ->East = false;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+        else if(MainBoard->Row3[i] == Piece && i==BoardSize){
+            Directions->North = true;
+            Directions->South = false;
+            Directions->West = false;
+            Directions ->East = true;
+            Directions->Row1 = true;
+            Directions->Row2 = false;
+            Directions->Row3 = true;
+        }
+    }
+    /*DEBUG:*/
+    printf("Allowed moves\nN:%d S:%d E:%d W:%d\n", Directions->North, Directions->South, Directions->East, Directions->West); 
+    printf("Allowed Rows: Row1:%d Row2:%d Row3:%d\n", Directions->Row1, Directions->Row2, Directions->Row3);
+    
+    return i;
 }
 
-void SaveGame(){
+void MovePiece(char Piece, char Direction, gameboard *MainBoard){
+    char PieceMoved = '1';
+    int Index = 0;
+    allowed *AllowedMovement = calloc(1, sizeof(allowed));
+    scanf(" %c", &Piece);
+    scanf(" %c", &Direction);
+    Index = ValidateMovement(AllowedMovement, MainBoard, Piece);
+    
+    //add directon small caps only
+    //validate that second row can not be moved
+    switch(Direction){
+        case 'n':
+        if (AllowedMovement->North==true){
+            if(MainBoard->Row3[Index]=='-'){
+                MainBoard->Row3[Index] = MainBoard->Row1[Index];
+                MainBoard->Row1[Index] = '-';
+            }
+            else{
+                printf("Illegal move selected! You can hop only over a non-empty position and land on an empty one!\n\n");
+            }
+        }
+        break;
 
+        case 's':
+        if (AllowedMovement->South==true){
+            if(MainBoard->Row1[Index]=='-'){
+                MainBoard->Row1[Index] = MainBoard->Row3[Index];
+                MainBoard->Row3[Index] = '-';
+            }
+            else{
+                printf("Illegal move selected! You can hop only over a non-empty position and land on an empty one!");
+            }
+        }
+        break;
+
+        case 'e':
+        if (AllowedMovement->East==true && AllowedMovement->Row1==true){
+            if(MainBoard->Row1[Index-2]=='-'){
+                MainBoard->Row1[Index-2] = MainBoard->Row1[Index];
+                MainBoard->Row1[Index] = '-';
+            }
+        }
+        else if (AllowedMovement->East==true && AllowedMovement->Row3==true){
+            if(MainBoard->Row3[Index-2]=='-'){
+                MainBoard->Row3[Index-2] = MainBoard->Row3[Index];
+                MainBoard->Row3[Index] = '-';
+            }
+        }
+        else{
+            printf("Illegal move selected! You can hop only over a non-empty position and land on an empty one!");
+        }
+        break;
+
+        case 'w':
+        if (AllowedMovement->West==true && AllowedMovement->Row1==true){
+            if(MainBoard->Row1[Index+2]=='-'){
+                MainBoard->Row1[Index+2] = MainBoard->Row1[Index];
+                MainBoard->Row1[Index] = '-';
+            }
+        }
+        else if (AllowedMovement->West==true && AllowedMovement->Row3==true){
+            if(MainBoard->Row3[Index+2]=='-'){
+                MainBoard->Row3[Index+2] = MainBoard->Row3[Index];
+                MainBoard->Row3[Index] = '-';
+            }
+        }
+        else{
+            printf("Illegal move selected! You can hop only over a non-empty position and land on an empty one!");
+        }
+        break;
+    }        
+}
+    
+
+gameboard* SaveGame(gameboard *MainBoard, gameboard *SaveBoard){
+    SaveBoard = MainBoard;
+    return SaveBoard;
+}
+
+gameboard* LoadGame(gameboard *MainBoard, gameboard *SaveBoard){
+    MainBoard = SaveBoard;
+    return MainBoard;
 }
 
 gameboard* SetupGame(gameboard *Board){
     /*Creates a game board and returns pointer to it*/
     Board = calloc(3, sizeof(gameboard));
 
+    for(int i=0; i<BoardSize; i++){
+        Board->Row1[i] = '-';
+        Board->Row2[i] = 9+i;
+        Board->Row3[i] = 49+i;
+    }
     return Board;
 }
 
-void Printer(){
+void Rules(){
+    printf("Welcome to LoTR - Console Adventures! The rules of the game are simple:\n");
+    printf("-You need to help Frodo reach mount doom with the one ring.\n-He can defeat Saruman's orcs by jumping over them.\n");
+    printf("-He can jump over an orc only if there is a free spot on the other side (aka a dash sign  -)\n");
+    printf("-Frodo can jump one position vertically between the three paths or horizontally on the same path.\n");
+    printf("-Frodo's friends can help him by displacing enemy forces, following the same movement rules as himself, so that Frodo can take them by surprise.\n");
+    printf("-And don't forget: Frodo can use the one ring to turn back time to his previous move, but only three times!\n\n\n");
+    printf("o=|==============>   One ring to rule them all and into darkness bind them   <==============|=o\n\n\n");
 
+    /* Using print raw string literall printf (R"()") is a c11 feature*/
+    printf(R"(
+                              \ : /
+                    '-: __ :-'
+                    -:  )(_ :--
+                    -' |r-_i'-
+            ,sSSSSs,   (2-,7
+            sS';:'`Ss   )-j
+           ;K e (e s7  /  (
+            S, ''  SJ (  ;/
+            sL_~~_;(S_)  _7
+|,          'J)_.-' />'-' `Z
+j J         /-;-A'-'|'--'-j\
+ L L        )  |/   :    /  \
+  \ \       | | |    '._.'|  L
+   \ \      | | |       | \  J
+    \ \    _/ | |       |  ',|
+     \ L.,' | | |       |   |/
+    _;-r-<_.| \=\    __.;  _/
+      {_}"  L-'  '--'   / /|
+            |   ,      |  \|
+            |   |      |   ")
+            L   ;|     |   /|
+           /|    ;     |  / |
+          | |    ;     |  ) |
+         |  |    ;|    | /  |
+         | ;|    ||    | |  |
+         L-'|____||    )/   |
+             % %/ '-,- /    /
+     snd     |% |   \%/_    |
+          ___%  (   )% |'-; |
+        C;.---..'   >%,(   "'
+                   /%% /
+                  Cccc')");
+
+    printf("\n\n\n");
+}
+
+void Printer(){
+    printf("Who do you want to move (Frodo - figure 1 or any enemy - other numbers?\n");
+    printf("Do hurry! The Nine have left Minas Morgul!\n");
+    printf("Choose one number and thern a direction (N/S/E/W)\n");
+}
+
+void PrintBoard(gameboard *MainBoard){
+    for (int i=0; i<BoardSize; i++){
+        printf("%2c\t", MainBoard->Row1[i]);
+    }
+    printf("\n");
+
+    for (int i=0; i<BoardSize; i++){
+        printf("%2d\t", MainBoard->Row2[i]);
+    }
+    printf("\n");
+
+    for (int i=0; i<BoardSize; i++){
+        printf("%2c\t", MainBoard->Row3[i]);
+    }
+    printf("\n");
+}
+
+char ContinueGame (char Continue){
+    
+    CONTINUE: 
+    printf("Frodo wouldn't have got far without Sam, would he, dad? (Continue: y/n)\n");
+    scanf(" %c", &Continue);
+    
+    if (Continue == 'y' || Continue =='n'){
+        printf("Now, Mr. Frodo, you shouldn't make fun. I was serious.\n");
+        return Continue;
+    }
+    else{
+        goto CONTINUE;
+    }
 }
--- INClass.c
+++ INClass.c
@@ -1,92 +1,73 @@
-*Опишете времето в структура: часове:минути:секунди като структура tagTMyTime.
-Използвайки тази структура, дефинирайте следните функции: добавяне на секунди,
-добавяне на минути. Добавяне на часове към дадена променлива от тип struct
-tagTMyTime. Напишете следните функции: връщане на броя секунди за дадена
-променлива от въведения тип и обратна функция от секундите да се генерира
-променлива tagTMyTime. Напишете две функции, които изваждат и събират две
-променливи от тип struct tagMyTime и връщат променлива от същия тип.
-Използвайте тези функции, за да се уверите, че работят коректно.*/
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct {
-    int hours;
-    int minutes;
-    int seconds;
 
 
-}tagTMyTime;
-void checkTime(tagTMyTime *time){
-    if(time->seconds>59){
-        time->minutes=time->seconds/60+time->minutes;
-        time->seconds=time->seconds%60;
-        }
-    if(time->minutes>59){
-            time->hours=time->minutes/60+time->hours;
-            time->minutes=time->minutes%60;
-        }
-    if (time->hours >23)
-    {   if(time->hours==24){
-        time->hours=0;
-    }else{
-        
-        time->hours = abs(time->hours-24);
-    }
-    }
-    
-};
-void addSeconds(tagTMyTime* time,int seconds){
-    time->seconds+=seconds;
-    checkTime(time);
-};
-void addMinutes(tagTMyTime *time,int minutes){
-    time->minutes+=minutes;
-    checkTime(time);
-};
 
-void addHours(tagTMyTime *time,int hours){
-    time->hours+=hours;
-    checkTime(time);
-
-};
-void printTime(tagTMyTime *time){
-    printf("%02d:%02d:%02d\n", time->hours, time->minutes, time->seconds);
-}
-tagTMyTime add(tagTMyTime *time,tagTMyTime *time1,tagTMyTime *time2){
-    
-    time2->seconds=time->seconds+time1->seconds;
-    time2->minutes=time->minutes+time1->minutes;
-    time2->hours=time->hours+time1->hours;
-    checkTime(time2);
-
-   
-}
 
 int main(){
-    
+int choice;
+int  choice1;
+
+int i,j;
+int SOLITARE[3][8]={{45,45,45,45,45,45,45,45},
+                {9,10,11,12,13,14,15,16},
+                {1, 2, 3, 4, 5, 6, 7 , 8,}
+                };
+for(i=0;i<3;i++){
+    for(j=0;j<8;j++){
+    printf("\t%d ",SOLITARE[i][j]);
+    }
+    printf("\n");
+}
+while(1){
+
+printf("chose a number from 1 to 16 to move\n");
+scanf(" %d",&choice);
+
+
+for(i=0;i<3;i++){
+
+    for(j=0;j<8;j++){
+        if(SOLITARE[i][j]==choice){
+           
+            break;
+        }
+        
+    }
    
-    tagTMyTime *clock=NULL;
-    clock=(tagTMyTime*)malloc(sizeof(tagTMyTime));
-    tagTMyTime *clock1=NULL;
-    clock1=(tagTMyTime*)malloc(sizeof(tagTMyTime));
-    clock->hours=20;
-    clock->minutes=32;
-    clock->seconds=34;
-    addSeconds(clock,42);
-    printTime(clock);
-    addMinutes(clock,62);
-    printTime(clock);
-    addHours(clock,2);
-    printTime(clock);
-    clock1->hours=15;
-    clock1->minutes=21;
-    clock1->seconds=35;
-    tagTMyTime *clock2=NULL;
-    clock2=(tagTMyTime*)malloc(sizeof(tagTMyTime));
+   }
+printf("arr[%d][%d]\n",i,j);
+scanf(" %d",&choice1);
+
+switch (choice1)
+{
+
+
+case 1:/*nadqsno*/
+   
+   printf("arr[%d][%d]\n",i,j);
+    j--;
+    SOLITARE[i][j+2]=SOLITARE[i][j];
+    SOLITARE[i][j]=45;
+    SOLITARE[i][j+1]=45;
+    break;
+
+    case 2:/*nagore*/
+    i--;
+    printf("arr[%d][%d]\n",i,j);
+    SOLITARE[i-2][j]=SOLITARE[i][j];
+    SOLITARE[i-1][j]=45;   
+    SOLITARE[i][j]=45;
     
-    add(clock, clock1,clock2);
-    printTime(clock2);
-    free(clock);
-    free(clock1);
-    free(clock2);
+    break;
+
+}
+for( i=0;i<3;i++){
+    for(j=0;j<8;j++){
+    printf("\t%d ",SOLITARE[i][j]);
+    }
+    printf("\n");
+}
+}
+
+
 }
