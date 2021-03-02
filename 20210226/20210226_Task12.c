876dd01e61e9b355d3a83e10898209a4b1d510f7
Iordan Tonchev <73355148+issashu@users.noreply.github.com>
2021-03-02 08:38:30 +0200

    26022021 task 16

--- 20210226/20210226_Task12.c
+++ 20210226/20210226_Task12.c
@@ -1,3 +1,15 @@
+/*
+Задача 12. Представете служителите във фирма в структура с
+членове: пореден номер, име, презиме, фамилия, позиция, трудов стаж в
+години, заплата в лева, указател към структурата, описващ неговия
+началник. Напишете програма, която въвежда 10 служителя, които се
+съхраняват в масив от описаните структури. Въвеждането на служителите
+може да стане на два паса, първо въвеждане на всички данни без
+указателя към началника и на втори пас, указване на всеки служител кой е
+неговият началник. В решението трябва да се използва динамично
+заделяне на памет и typedef.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -11,37 +23,53 @@ typedef struct information{
     char *position;
     int exp;
     float salary;
+ }information;
+
+void enterInformation(information *array);
+void printList(information *array,information *manager);
+
+int main(){
+    information *array,*manager;
+    array=manager=NULL;
+    array=malloc(listSize*sizeof(information));
+    manager=malloc(sizeof(information));
+    manager->number=1;
+    manager->firstName="Ivan";
+    manager->secondName="Ivanov";
+    manager->thirdName="Ivanov";
+    manager->position="ceo";
+    manager->salary=2500;
+    manager->exp=25;
+
+    enterInformation(array);
+    printList(array,manager);
     
+    free(array);
+    free(manager);
+}
 
-
-}information;
-
-
-void enterInformation(information *array)
-{
+void enterInformation(information *array){
     for(int index=0;index<listSize;index++ ){
-    printf("Employee Informattion\n");
-    printf("Enter employee number: ");
-    scanf(" %d", &array[index].number);
-    printf("Enter employee first name:");
-    scanf("%s", &array[index].firstName);
-    printf("Enter employee secomd:");
-    scanf("%s", &array[index].secondName);
-    printf("Enter employee third name:");
-    scanf("%s", &array[index].thirdName);
-    printf("Enter employee position:");
-    scanf("%s", &array[index].position);
-    printf("Enter employee work experience:");
-    scanf("%d", &array[index].exp);
-    printf("Enter employee salary:");
-    scanf("%float", &array[index].salary);
+        printf("Employee Informattion\n");
+        printf("Enter employee number: ");
+        scanf(" %d", &array[index].number);
+        printf("Enter employee first name:");
+        scanf(" %s", &array[index].firstName);
+        printf("Enter employee middle name:");
+        scanf(" %s", &array[index].secondName);
+        printf("Enter employee family name:");
+        scanf(" %s", &array[index].thirdName);
+        printf("Enter employee position:");
+        scanf(" %s", &array[index].position);
+        printf("Enter employee work experience:");
+        scanf(" %d", &array[index].exp);
+        printf("Enter employee salary:");
+        scanf(" %f", &array[index].salary);
     }
+}
 
-};
-void printList(information *array,information *manager)
-{
-    for (int i = 0; i < listSize; i++)
-    {
+void printList(information *array,information *manager){
+    for (int i = 0; i < listSize; i++){
         printf("=============================\n");
         printf("------Employee Details-------\n");
         printf("Employee number: %d\n", array[i].number);
@@ -55,33 +83,5 @@ void printList(information *array,information *manager)
         printf("Manager position: %s\n", manager->position);
         printf("Manager work experience: %d\n", manager->exp);
         printf("Manager salary: %.2f\n", manager->salary);
-        
     }
-};
-
-
-int main(){
-
-    
-    information *array,*manager;
-    array=manager=NULL;
-    array=malloc(listSize*sizeof(information));
-    manager=malloc(sizeof(information));
-    manager->number=1;
-    manager->firstName="Ivan";
-    manager->secondName="Ivanov";
-    manager->thirdName="Ivanov";
-    manager->position="ceo";
-    manager->salary=2500;
-    manager->exp=25;
-
-    
-    
-    enterInformation(array);
-    printList(array,manager);
-    
-    free(array);
-    free(manager);
-
-    
 }
--- 20210226/20210226_Task16_2.c
+++ 20210226/20210226_Task16_2.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <ctype.h>
 
+void testFunP(int SOLITARE[][8]);
+void testFun(int SOLITARE[][8]);
+
+
+int main(){
+    int SOLITARE [3][8]={{45,45,45,45,45,45,45,45},
+                         {9,10,11,12,13,14,15,16},
+                         {1, 2, 3, 4, 5, 6, 7, 8,}
+                        };
+    testFunP(SOLITARE);
+
+    while(1){   
+        testFun(SOLITARE);
+    }
+}
 
 void testFunP(int SOLITARE[][8]){
     
@@ -17,7 +32,6 @@ void testFunP(int SOLITARE[][8]){
     }
 }
 
-
 void testFun(int SOLITARE[][8]){
     int indexI, indexJ;
     static int backUp[3][8];
@@ -28,113 +42,81 @@ void testFun(int SOLITARE[][8]){
             backUp[i][j]=SOLITARE[i][j];
        }
     }
-printf("Chose a number from 1 to 16 to move\n");
-scanf(" %d",&choice);
+    printf("Chose a number from 1 to 16 to move\n");
+    scanf(" %d",&choice);
 
-if(isalpha(choice1) == 0){
-    for(int i=0;i<3;i++){
-        for(int j=0;j<8;j++){
-            if(SOLITARE[i][j]==choice){
-                indexI=i;
-                indexJ=j;
-                break;
-            }    
+    if(isalpha(choice1) == 0){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<8;j++){
+                if(SOLITARE[i][j]==choice){
+                    indexI=i;
+                    indexJ=j;
+                    break;
+                }    
+            }
         }
-   }
+        scanf(" %d",&choice1);
 
+        switch (choice1){
+            case 1:
+            /*nadqsno*/
+                if(indexJ+2>7 || SOLITARE[indexI][indexJ+2]!=45 || SOLITARE[indexI][indexJ+1]==45|| SOLITARE[indexI][indexJ+1]==1 ){
+                    printf("chose different path\n");
+                }
+                else{
+                    SOLITARE[indexI][indexJ+2]=SOLITARE[indexI][indexJ];
+                    SOLITARE[indexI][indexJ]=45;
+                    SOLITARE[indexI][indexJ+1]=45;
+                }
+            break;
+
+            case 2:
+            /*nagore*/
+                if(indexI-2<0 || SOLITARE[indexI-2][indexJ]!=45 || SOLITARE[indexI-1][indexJ]==45 || SOLITARE[indexI-1][indexJ]==1){
+                    printf("chose different path\n");
+                }
+                else{
+                    SOLITARE[indexI-2][indexJ]=SOLITARE[indexI][indexJ];
+                    SOLITARE[indexI-1][indexJ]=45;   
+                    SOLITARE[indexI][indexJ]=45;
+                }
+            break;
+
+            case 3:
+            /*nadolu*/
+                if(indexI+2>2 || SOLITARE[indexI+2][indexJ]!=45 || SOLITARE[indexI+1][indexJ]==45 || SOLITARE[indexI+1][indexJ]==1){
+                    printf("chose different path\n");
+                }
+                else{
+                    SOLITARE[indexI+2][indexJ]=SOLITARE[indexI][indexJ];
+                    SOLITARE[indexI+1][indexJ]=45;   
+                    SOLITARE[indexI][indexJ]=45;
+                }
+            break;
+
+            case 4: 
+            /*nalqvo*/  
+                if(indexJ-2<0||SOLITARE[indexI][indexJ-2]!=45 || SOLITARE[indexI][indexJ-1]==45 || SOLITARE[indexI][indexJ-1]==1 ){
+                    printf("chose different path\n");
+                }
+                else{
+                    SOLITARE[indexI][indexJ-2]=SOLITARE[indexI][indexJ];
+                    SOLITARE[indexI][indexJ]=45;
+                    SOLITARE[indexI][indexJ-1]=45;
+                }
+            break;
+        }
+    }   
+
+    testFunP(SOLITARE);
+    printf("do you want to rewind your board\n");
     scanf(" %d",&choice1);
-
-switch (choice1)
-{
-
-
-case 1:/*nadqsno*/
-   
-  
-    if(indexJ+2>7 || SOLITARE[indexI][indexJ+2]!=45 || SOLITARE[indexI][indexJ+1]==45|| SOLITARE[indexI][indexJ+1]==1 ){
-        printf("chose different path\n");
-
+    if(choice1==5){
+        for(int i=0;i<3;i++){
+            for(int j=0;j<8;j++){
+                SOLITARE[i][j]=backUp[i][j];
+            }
+        }
+        testFunP(SOLITARE);
     }
-    else{
-    SOLITARE[indexI][indexJ+2]=SOLITARE[indexI][indexJ];
-    SOLITARE[indexI][indexJ]=45;
-    SOLITARE[indexI][indexJ+1]=45;};
-    break;
-
-    case 2:/*nagore*/
-    
-    if(indexI-2<0 || SOLITARE[indexI-2][indexJ]!=45 || SOLITARE[indexI-1][indexJ]==45 || SOLITARE[indexI-1][indexJ]==1){
-        printf("chose different path\n");
-
-    }
-    else{
-
-    SOLITARE[indexI-2][indexJ]=SOLITARE[indexI][indexJ];
-    SOLITARE[indexI-1][indexJ]=45;   
-    SOLITARE[indexI][indexJ]=45;
-    };
-    
-    break;
-    case 3:/*nadolu*/
-    if(indexI+2>2 || SOLITARE[indexI+2][indexJ]!=45 || SOLITARE[indexI+1][indexJ]==45 || SOLITARE[indexI+1][indexJ]==1){
-        printf("chose different path\n");
-
-    }
-    else{
-    
-    SOLITARE[indexI+2][indexJ]=SOLITARE[indexI][indexJ];
-    SOLITARE[indexI+1][indexJ]=45;   
-    SOLITARE[indexI][indexJ]=45;};
-    break;
-
-    case 4: /*nalqvo*/
-    if(indexJ-2<0||SOLITARE[indexI][indexJ-2]!=45 || SOLITARE[indexI][indexJ-1]==45 || SOLITARE[indexI][indexJ-1]==1 ){
-        printf("chose different path\n");
-    }
-    else{
-    SOLITARE[indexI][indexJ-2]=SOLITARE[indexI][indexJ];
-    SOLITARE[indexI][indexJ]=45;
-    SOLITARE[indexI][indexJ-1]=45;};
-    break;
-
-    /*case 5:/*rewind
-    for(int i=0;i<3;i++){
-       for(int j=0;j<8;j++){
-        SOLITARE[i][j]=backUp[i][j];
-       }
-   }
-   break;
-    */
 }
-}
-
-
-testFunP(SOLITARE);
-printf("do you want to rewind your board\n");
-scanf(" %d",&choice1);
-if(choice1==5){
-for(int i=0;i<3;i++){
-       for(int j=0;j<8;j++){
-         SOLITARE[i][j]=backUp[i][j];
-       }
-   }
-   testFunP(SOLITARE);
-}
-
-
-}
-
-
-int main(){
-
-int SOLITARE [3][8]={{45,45,45,45,45,45,45,45},
-                {9,10,11,12,13,14,15,16},
-                {1, 2, 3, 4, 5, 6, 7 , 8,}
-                };
-testFunP(SOLITARE);
-
-while(1){
-
-testFun(SOLITARE);
-}
-};
