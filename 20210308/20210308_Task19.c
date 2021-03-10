0b10b053186ef9d0841670e215ab0602700f4197
Iordan Tonchev <73355148+issashu@users.noreply.github.com>
2021-03-10 08:28:12 +0200

    08032021 update

--- 20210308/20210308_Task19.c
+++ 20210308/20210308_Task19.c
@@ -9,7 +9,48 @@
 #include <stdio.h>
 #include <stdio.h>
 
-int main(){
-FILE *FilePoionter = NULL;
+int CatFunct(char** argv);
+void ErrorHandler(int ErrNum);
 
+int main(int argc, char** argv){
+    int FuncResult = 0;
+    if (argc==1){
+        printf("Not implemented yet!");
+        return 0;
+    }
+    else if(argc>2){
+        printf("Usage cat <filename.extension>\n");
+    }
+    else if(argc == 2){
+        FuncResult = CatFunct(argv[1]);
+        ErrNum(FuncResult);
+    }
+    
+}
+
+int CatFunct((char** argv){
+    FILE *FilePoionter = NULL;
+    char *filename = NULL;
+    scanf("cat %s", filename);
+    FilePoionter = fopen(filename,"r");
+    if(FilePoionter==NULL){
+        return 404;
+    }
+    else {
+        fprintf(stdin,"%s",FilePoionter);
+        return 0;
+    }
+}
+
+void ErrorHandler(int ErrNum){
+    switch (ErrNum){
+    case 404:
+        printf("404.File not found.");
+        printf("Usage cat <filename.extension>\n");
+    break;
+
+    case 0:
+        printf("\nHope you got what you wanted. Have a nice day!\n");
+    break;
+    }
 }
