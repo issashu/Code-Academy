eadc969aaa40737bb4f155931bdd9ee7e03651f1
Iordan Tonchev <issashu@icloud.com>
2021-03-14 18:05:27 +0200

    makefile initial commit

--- Personal Homework/src/main.c
+++ Personal Homework/src/main.c
@@ -1,4 +1,22 @@
+/*
+Напишете програма, която добавя и изтрива елемент от списъка по зададена стойност, която се пази в него. Използвайте следния елемент на двойно-свързания списък:
+ 
+doubly linked list
+
+typedef struct TDListNode { 
+  int m_nValue; 
+  struct TDListNode* m_pNextNode; 
+  struct TDListNode* m_pPrevNode; 
+} TDListNode;
+*/
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TDListNode { 
+  int m_nValue; 
+  struct TDListNode* m_pNextNode; 
+  struct TDListNode* m_pPrevNode; 
+} TDListNode;
 
 int main()
 {
